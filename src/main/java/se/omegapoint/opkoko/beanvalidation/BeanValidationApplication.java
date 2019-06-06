bb33582dc85e9604f17f715c504d9c3c9b4e80a5
Joakim Glimsjö <joakim.glimsjo@omegapoint.se>
2019-06-06 19:59:24 +0200

    Initial commit

---
+++ src/main/java/se/omegapoint/opkoko/beanvalidation/BeanValidationApplication.java
@@ -0,0 +1,12 @@
+package se.omegapoint.opkoko.beanvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanValidationApplication.class, args);
+    }
+}
---
+++ src/test/java/se/omegapoint/opkoko/beanvalidation/BeanValidationApplicationTests.java
@@ -0,0 +1,15 @@
+package se.omegapoint.opkoko.beanvalidation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeanValidationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
