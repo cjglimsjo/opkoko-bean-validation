ae7e3ab34c235c91241d445c8ab66e8627fea1de
Joakim Glimsjö <joakim.glimsjo@omegapoint.se>
2019-06-07 05:08:11 +0200

    Add ExceptionHandler

---
+++ src/main/java/se/omegapoint/opkoko/beanvalidation/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,30 @@
+package se.omegapoint.opkoko.beanvalidation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.stream.Collectors;
+
+@Slf4j
+@ControllerAdvice
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        String fieldErrors = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + " " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        log.error("Invalid request: " + fieldErrors);
+        return handleExceptionInternal(ex, null, headers, status, request);
+    }
+}
