c3e5e2c93a56d5b4e0ed7b686fe2c2f44b0f04d2
Joakim Glimsjö <joakim.glimsjo@omegapoint.se>
2019-06-10 22:12:05 +0200

    Use custom constraints

--- src/main/java/se/omegapoint/opkoko/beanvalidation/orders/Order.java
+++ src/main/java/se/omegapoint/opkoko/beanvalidation/orders/Order.java
@@ -2,12 +2,12 @@ package se.omegapoint.opkoko.beanvalidation.orders;
 
 import lombok.Getter;
 import lombok.ToString;
+import se.omegapoint.opkoko.beanvalidation.constraints.Alphanumeric;
+import se.omegapoint.opkoko.beanvalidation.constraints.DateFormat;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @Getter
@@ -15,10 +15,12 @@ import java.util.List;
 public class Order {
 
     @NotNull
-    @Pattern(regexp = "^[A-Za-z0-9]{10}$")
+    //@Pattern(regexp = "^[A-Za-z0-9]{10}$")
+    @Alphanumeric
     private String orderNumber;
 
-    @NotBlank
+    //@NotBlank
+    @DateFormat
     private String creationDate;
 
     @NotNull
--- src/main/java/se/omegapoint/opkoko/beanvalidation/orders/OrderLine.java
+++ src/main/java/se/omegapoint/opkoko/beanvalidation/orders/OrderLine.java
@@ -2,16 +2,15 @@ package se.omegapoint.opkoko.beanvalidation.orders;
 
 import lombok.Getter;
 import lombok.ToString;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
+import se.omegapoint.opkoko.beanvalidation.constraints.Range;
 
 @Getter
 @ToString
 public class OrderLine {
 
-    @Min(1)
-    @Max(99)
+    //@Min(1)
+    //@Max(99)
+    @Range(min = 1, max = 99)
     private Long quantity;
 
     // ...
